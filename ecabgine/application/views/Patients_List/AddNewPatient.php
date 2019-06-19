ba1d18fa1768ca5a7587b9a20db56a974079e7e5
Calin Cocan <calin.iap@gmail.com>
2019-06-19 11:15:05 +0300

    AddNewPatient.php has been clenad by debug code

--- ecabgine/application/views/Patients_List/AddNewPatient.php
+++ ecabgine/application/views/Patients_List/AddNewPatient.php
@@ -1,50 +1,5 @@
 <h2><?php echo $title; ?></h2>
 
- <?php
-	// foreach ($county_list as $county)
-	// {
-	// 	// echo '<option value="'.$county[0].'"> '.$county[0].$county[1].' </option>';
-	// 	$jud = 
-	// 	echo $county['id_county'].' '.$county['county'] . '<br>';
-	// 	print_r($county);
-	// 	echo '<br>';
-	// 	// echo $county[0].' '.$county[1] . '<br>';
-	// 	echo '<br>';
-	// }
-
- 	$county_values=array_values($county_list);
- 	$county_keys=array_keys($county_list);
- 	echo $county_keys[1];
-
- 	// foreach ($county_values as $value) {
- 	// 	// echo "$key <br>";
- 	// 	print_r($value);
- 	// 	echo '<br>';
- 	// }
-
-
- 	// while($county=current($county_values))
- 	// {
- 	// 	echo $county.' <br>';
- 	// 	next($county_values);
- 	// }
-
-
-	// foreach ($county_list as $county)
-	// {
-	// 	list($id, $value)=$county;
-	// 	echo $id.' '.$value.'<br>';
-	// }
-	// while ($name = current($county_list)) 
- //    {
-
- //        echo key($county_list).' ';
- //        print_r(current($county_list));
- //        echo '<br />';
- //        next($county_list);
- //    }
-?>
-
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('Patients_List/insert_new_patient'); ?>
@@ -61,10 +16,6 @@
 		<label for="patient_id">Patient ID</label>
 		<input type="text" name="patient_id"><br />
 
-<!-- this 2 lines are for debug. we'll delete later -->
-<!-- 		<label for="county">County</label>
-		<input type="number" name="county"><br />
- -->
 		<label for="county">Select county</label>
 		<select name="county">
 		<?php
@@ -74,21 +25,7 @@
 			}
 		?>
 		</select> <br/>
-<!-- this 2 lines are for debug. we'll delete later -->
-<!-- 		<label for="location">Location</label>
-		<input type="number" name="location"><br />	
- -->
- <!-- 		<label for="select_city">Select city</label>
-		<select name="select_city">
-		<?php
-			foreach ($city_list as $city)
-				{
-					echo '<option value="'.$city['id_city'].'"> '.$city['id_city'].' '.$city['city'].' </option>';
-				}
 
-		?>
-		</select><br>
- -->
  		<label for="location">Select locality</label>
 		<select name="location">
 		<?php
@@ -99,7 +36,6 @@
 
 		?>
 		</select><br>
-
 
 		<label for="address">Address</label>
 		<input type="text" name="address"><br />	
